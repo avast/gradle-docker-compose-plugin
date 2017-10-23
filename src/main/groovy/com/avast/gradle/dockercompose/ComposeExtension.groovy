f8a183ed9c13e8a04382d8a6773ea04845da3f92
Michal Augustyn <augustyn@avast.com>
2017-10-23 10:53:58 +0200

    refactor ComposeExtension for better readability

--- src/main/groovy/com/avast/gradle/dockercompose/ComposeExtension.groovy
+++ src/main/groovy/com/avast/gradle/dockercompose/ComposeExtension.groovy
@@ -10,18 +10,27 @@ class ComposeExtension extends ComposeSettings {
 
     private HashMap<String, ComposeSettings> settings = [:]
 
+    private ComposeSettings getOrCreateNested(String name) {
+        settings.computeIfAbsent(name, { createNested(name) })
+    }
+
     def methodMissing(String name, def args) {
-        if (name.startsWith('isRequiredBy') && args[0] instanceof String) {
+        // If the method name is 'isRequiredByXXX' then the name of nested configuration will be XXX
+        // and we will call isRequiredBy(XXX) for the newly created nested configuration.
+        // The method must have one parameter that is path to the Docker Compose file.
+        if (name.startsWith('isRequiredBy') && args[0]) {
             def taskName = name.substring('isRequiredBy'.length())
             taskName = taskName[0].toLowerCase() + taskName.substring(1)
-            ComposeSettings s = settings.computeIfAbsent(name, { createNested(taskName) })
-            s.useComposeFiles = [args[0] as String]
+            ComposeSettings s = getOrCreateNested(taskName)
+            s.useComposeFiles = [args[0].toString()]
             project.tasks.findAll { it.name.equalsIgnoreCase(taskName) }.forEach { s.isRequiredBy(it) }
             s
-        } else {
-            ComposeSettings s = settings.computeIfAbsent(name, { createNested(name) })
+        } else if (args[0] instanceof Closure) {
+            ComposeSettings s = getOrCreateNested(name)
             ConfigureUtil.configure(args[0] as Closure, s)
             s
+        } else {
+            getOrCreateNested(name)
         }
     }
 }
