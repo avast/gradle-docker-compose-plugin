f0897f77651ba7df559522f9bb2e24dfd575b1d6
Michal Augustýn <augustyn.michal@gmail.com>
2017-10-26 16:53:36 +0200

    allow to access the nested settings using empty parenthesis syntax

--- src/main/groovy/com/avast/gradle/dockercompose/ComposeExtension.groovy
+++ src/main/groovy/com/avast/gradle/dockercompose/ComposeExtension.groovy
@@ -18,14 +18,14 @@ class ComposeExtension extends ComposeSettings {
         // If the method name is 'isRequiredByXXX' then the name of nested configuration will be XXX
         // and we will call isRequiredBy(XXX) for the newly created nested configuration.
         // The method must have one parameter that is path to the Docker Compose file.
-        if (name.startsWith('isRequiredBy') && args[0]) {
+        if (name.startsWith('isRequiredBy') && args.length == 1 && args[0]) {
             def taskName = name.substring('isRequiredBy'.length())
             taskName = taskName[0].toLowerCase() + taskName.substring(1)
             ComposeSettings s = getOrCreateNested(taskName)
             s.useComposeFiles = [args[0].toString()]
             project.tasks.findAll { it.name.equalsIgnoreCase(taskName) }.forEach { s.isRequiredBy(it) }
             s
-        } else if (args[0] instanceof Closure) {
+        } else if (args.length == 1 && args[0] instanceof Closure) {
             ComposeSettings s = getOrCreateNested(name)
             ConfigureUtil.configure(args[0] as Closure, s)
             s
