1b072bffd4e6501f4d24714ee493848cf48802cd
Michal Augustyn <augustyn.michal@gmail.com>
2018-02-18 11:01:45 +0100

    remove overloaded field

--- src/main/groovy/com/avast/gradle/dockercompose/tasks/ComposeDown.groovy
+++ src/main/groovy/com/avast/gradle/dockercompose/tasks/ComposeDown.groovy
@@ -1,11 +1,8 @@
 package com.avast.gradle.dockercompose.tasks
 
-import com.avast.gradle.dockercompose.ComposeSettings
 import org.gradle.api.tasks.TaskAction
 
 class ComposeDown extends ComposeDownForced {
-    ComposeSettings settings
-
     ComposeDown() {
         group = 'docker'
         description = 'Stops and removes containers of docker-compose project (only if stopContainers is set to true)'
