b3567a0ab2e9fb11df51e0d3fbbd28ca0f08d80d
Michal Augustyn <augustyn.michal@gmail.com>
2018-10-03 14:30:50 +0200

    option to ignore TCP ports when waiting

--- src/main/groovy/com/avast/gradle/dockercompose/tasks/ComposeUp.groovy
+++ src/main/groovy/com/avast/gradle/dockercompose/tasks/ComposeUp.groovy
@@ -156,8 +156,10 @@ class ComposeUp extends DefaultTask {
     void waitForOpenTcpPorts(Iterable<ServiceInfo> servicesInfos) {
         def start = Instant.now()
         servicesInfos.forEach { serviceInfo ->
-            serviceInfo.containerInfos.each { instanceName, containerInfo ->
-                containerInfo.tcpPorts.forEach { exposedPort, forwardedPort ->
+            serviceInfo.containerInfos..each { instanceName, containerInfo ->
+                containerInfo.tcpPorts
+                .findAll { ep, fp -> !settings.tcpPortsToIgnoreWhenWaiting.any { it == ep } }
+                .forEach { exposedPort, forwardedPort ->
                     logger.lifecycle("Probing TCP socket on ${containerInfo.host}:${forwardedPort} of service '${instanceName}'")
                     while (true) {
                         try {
