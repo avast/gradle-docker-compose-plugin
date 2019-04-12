b16465ef868060e1a8ccff9235f8295d7b12a01f
Jan Monterrubio <Jan.Monterrubio@Cerner.com>
2019-04-11 22:52:47 -0500

    add config parsing

---
+++ src/test/groovy/com/avast/gradle/dockercompose/ComposeConfigParserTest.groovy
@@ -0,0 +1,183 @@
+package com.avast.gradle.dockercompose
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * Verifies behavior of ComposeConfigParser
+ */
+class ComposeConfigParserTest extends Specification
+{
+
+    def "findServiceDependencies with a service two direct dependencies in version 3" ()
+    {
+        given: "compose config output for a service"
+        def configOutput = """
+            services:
+              master:
+                depends_on:
+                  - slave0
+                  - slave2
+              slave0:
+                expose:
+                  - '22'
+              slave1:
+                expose:
+                  - '23'
+            version: '3.0'
+        """
+
+        when: "findServiceDependencies is called"
+        def dependenciesMap = ComposeConfigParser.findServiceDependencies(configOutput)
+
+        then: "master has two dependencies"
+        dependenciesMap["master"].size() == 2
+
+        and: "slave0 has no dependencies"
+        dependenciesMap["slave0"].isEmpty()
+
+        and: "slave1 has no dependencies"
+        dependenciesMap["slave1"].isEmpty()
+    }
+
+    def "findServiceDependencies with a service two direct dependencies in version 1" ()
+    {
+
+        given: "compose config output for a service"
+        def configOutput = """
+        master:
+          links:
+            - slave0
+            - slave2
+        slave0:
+            expose:
+              - '22'
+        slave1:
+            expose:
+              - '23'
+        """
+
+        when: "findServiceDependencies is called"
+        def dependenciesMap = ComposeConfigParser.findServiceDependencies(configOutput)
+
+        then: "master has two dependencies"
+        dependenciesMap["master"].size() == 2
+
+        and: "slave0 has no dependencies"
+        dependenciesMap["slave0"].isEmpty()
+
+        and: "slave1 has no dependencies"
+        dependenciesMap["slave1"].isEmpty()
+    }
+
+    def "findServiceDependencies with a service 4 indirect dependencies in version 3" ()
+    {
+        given: "compose config output for a service"
+        def configOutput = """
+            services:
+              db:
+                expose:
+                  - 1414
+              splunkForward:
+                expose:
+                  - 8444
+              dataService:
+                depends_on:
+                  - db
+                expose:
+                  - '8080'
+              audit:
+                depends_on:
+                  - splunkForward
+              ui:
+                depends_on:
+                  - dataService
+                  - audit
+                expose:
+                  - '23'
+            version: '3.0'
+        """
+
+        when: "findServiceDependencies is called"
+        def dependenciesMap = ComposeConfigParser.findServiceDependencies(configOutput)
+
+        then: "ui has 4 dependencies (audit, splunkForward, dataService, db)"
+        dependenciesMap["ui"] == ["audit", "splunkForward", "dataService", "db"] as Set
+    }
+
+    def "findServiceDependencies with a service 4 indirect dependencies in version 1" ()
+    {
+        given: "compose config output for a service"
+        def configOutput = """
+              db:
+                expose:
+                  - 1414
+              splunkForward:
+                expose:
+                  - 8444
+              dataService:
+                links:
+                  - db
+                expose:
+                  - '8080'
+              audit:
+                links:
+                  - splunkForward
+              ui:
+                links:
+                  - dataService
+                  - audit
+                expose:
+                  - '23'
+        """
+
+        when: "findServiceDependencies is called"
+        def dependenciesMap = ComposeConfigParser.findServiceDependencies(configOutput)
+
+        then: "ui has 4 dependencies (audit, splunkForward, dataService, db)"
+        dependenciesMap["ui"] == ["audit", "splunkForward", "dataService", "db"] as Set
+    }
+
+    @Unroll
+    def "calculateDependenciesFromGraph computes dependencies for #service" ()
+    {
+        given: "a dependency graph"
+
+        /**
+         * services:
+         *   a:
+         *   b:
+         *     depends_on:
+         *       - a
+         *   c:
+         *     depends_on:
+         *       - b
+         *   d:
+         *   e:
+         *     depends_on:
+         *       - c
+         *       - d
+         */
+        def dependencyGraph = [
+                    "a":[],
+                    "b": ["a"],
+                    "c": ["b"],
+                    "d": [],
+                    "e": ["c", "d"]
+        ]
+
+        when: "calculateDependenciesFromGraph is called for #service"
+        def dependencies = ComposeConfigParser.calculateDependenciesFromGraph(service, dependencyGraph)
+
+        then: "the service's dependency set is calculated correctly"
+        dependencies == expectedSet
+
+        where:
+        service | expectedSet
+        "a" | [] as Set
+        "b" | ["a"] as Set
+        "c" | ["a", "b"] as Set
+        "d" | [] as Set
+        "e" | ["a", "b", "c", "d"] as Set
+    }
+}
