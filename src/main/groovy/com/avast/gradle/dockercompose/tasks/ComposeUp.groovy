6e24dede59bb3103e4b9ea3109bf7862bffd6517
Michal Augustýn <michal.augustyn@gendigital.com>
2023-09-11 10:31:57 +0200

    chore: more logging to debug failing test

--- src/main/groovy/com/avast/gradle/dockercompose/tasks/ComposeUp.groovy
+++ src/main/groovy/com/avast/gradle/dockercompose/tasks/ComposeUp.groovy
@@ -230,6 +230,7 @@ abstract class ComposeUp extends DefaultTask {
             }
             processesState = transformed.join('\t')
         } catch (Exception e) {
+            System.err.println(e)
             logger.warn("Cannot process JSON returned from 'docker compose ps --format json'", e)
         }
         processesState + composeExecutor.get().execute('config') + startedServices.get().join(',')
